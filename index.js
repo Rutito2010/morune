c0e85a6cafa772f069a54161da6aea7c2b1cf9cd
Rutito2010 <kakaroto11@live.com.ar>
2025-09-01 23:03:54 -0300

    first commi

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require("express")
 const path = require("path")
 const app = express()
 
-app.get("/moune_55246d10.html", (req, res) => {
-  res.sendFile(path.join(__dirname, "morune_97a41de3.html"));
+app.get("/morune_c497061e.html", (req, res) => {
+  res.sendFile(path.join(__dirname, "morune_c497061e.html"));
 });
 app.listen(8080, ()=> console.log("ok"))
